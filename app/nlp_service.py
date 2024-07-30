3104411a77abc4318e9d944c51c947bb03f8da2a
francobdq <099francobdq099@gmail.com>
2024-07-29 21:22:39 -0300

    nlp service implemented

--- app/nlp_service.py
+++ app/nlp_service.py
@@ -1,16 +1,32 @@
+from pandasai import Agent
+import os
 from fastapi import APIRouter, Request
+from pydantic import BaseModel
 import pandas as pd
+from dotenv import load_dotenv
 
+load_dotenv()
+API_KEY = os.getenv("PANDASAI_API_KEY")
 
 nlp_endpoint = APIRouter()
 
 data_path = 'data/data.csv'
 df = pd.read_csv(data_path)
 
+os.environ["PANDASAI_API_KEY"] = API_KEY
+
+def nlp(question):
+    agent = Agent(df)
+    response = agent.chat(question)
+    return response
+
+class NLPQuery(BaseModel):
+    question: str
 
 @nlp_endpoint.post("/")
-async def nlp_post(request: Request):
+async def nlp_post(request: Request, body: NLPQuery):
+    result = nlp(body.question)
     return {
-        "question": "",
-        "answer": ""
+        "question": body.question,
+        "answer": result
     }
